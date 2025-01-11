10806afa0184baec533fc41c94eb0cf3626f1213
Marcelo Ramos <marcelorcramos@gmail.com>
2025-01-11 12:11:05 +0000

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Marcelo Ramos on 11/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
